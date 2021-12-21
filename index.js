ff3ee14452926c09e591389aca97955368211c2b
Chelsea Fisher <chelseasfisher1@gmail.com>
2021-12-20 22:04:40 -0600

    commit tree 528e0c481da87eae06cfc100fda88f305b3180dd

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+var multiply = num1*num2;
+
+var random= 1;
+
+const num3 = 12;
+const num4 = 3;
+var mod = num3/num4;
+
+var max = 20;
+
